8ec9b9960b7ee52f70085414e27b0fb0a45f59d7
xder414 <xder414@gmail.com>
2025-01-14 07:46:44 +0700

    menambah file baru

---
+++ latihan/soal1.py
@@ -0,0 +1,10 @@
+#program membaca bilangan bulat positif
+angka=int(input('masukan angka:'))
+
+#menegecek
+if angka / 4==0:
+    print("kelipatan empat")
+else:
+    print("bukan kelipatan empat")
+
+
---
+++ latihan/soal2.py
@@ -0,0 +1,8 @@
+#program menentukan diskon
+
+belanja=float(input("masukan total belanja:"))
+diskon=float(input("masukan diskon:"))
+total_bayar=(input("masukan total bayar:"))
+
+# 
+    
