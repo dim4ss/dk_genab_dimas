480f34568223512d49405da1c75cf074fa534cf5
xder414 <xder414@gmail.com>
2025-01-15 11:54:01 +0700

    menambah perubahan file

--- latihan3/soal2.py
+++ latihan3/soal2.py
@@ -1,9 +1,9 @@
 #program menentukan hadiah
 belanja=int(input("masukan total harga belanja:"))
 
-if belanja > 1.500000:
+if belanja > 1.500000 and belanja < 5000000 :
     print("SELAMAT ANDA MENDAPAT TV BRACKET")
-elif belanja > 5000000:
+elif belanja > 5.000000:
     print("SELAMAT ANDA MENDAPATKAN SOUND BAR")
 else:
-    print("tidak ada hadiah")
+    print("tidak ada bonus")
