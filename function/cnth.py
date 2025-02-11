836e8869b70b4858df5e9152cec793f3de13d929
dim4ss <dwi.apriyani337@smk.belajar.id>
2025-02-11 07:43:29 +0700

    menambah materi baru

---
+++ function/cnth.py
@@ -0,0 +1,9 @@
+def cetakIentitasSiswa(nama,kelas,Jenis_kelamin):
+    print("Nama : ",nama)
+    print("Kelas : ",kelas)
+    print("Jenis Kelamin : ",Jenis_kelamin)
+#panggil fungsi cetak identitas siswa
+cetakIentitasSiswa("Jojo",'X-PPLG','Laki-laki')
+cetakIentitasSiswa("Joji",'X-PPLG','Laki-laki')
+cetakIentitasSiswa("Jejo",'X-PPLG','Laki-laki')
+cetakIentitasSiswa("Jeje",'X-PPLG','Laki-laki')
